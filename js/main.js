99addd02fe906f342a16a912732b7d5c2ad10d2b
TolKing <TolKing0101@gmail.com>
2022-04-04 16:31:14 +0500

    changed some stuff

--- js/main.js
+++ js/main.js
@@ -1,39 +1,91 @@
-class Calculator{
-    display
-    number
-    clean
-    return
-    plus
-    minus
-    divide
-    multiply
-    equal
-    constructor (elDisplay,elNumber,elClean,elreturn,elPlus,elMinus,elDivide,elMultiply,elEqual){
-        this.display = elDisplay;
-        this.number = elNumber;
-        this.clean = elClean;
-        this.return = elreturn;
-        this.plus = elPlus;
-        this.minus = elMinus;
-        this.divide = elDivide;
-        this.multiply = elMultiply;
-        this.equal = elEqual;
-    }
-}
-let calc = new Calculator(
-    display = document.querySelector('.text'),
+// class Calculator{
+//     display
+//     number
+//     clean
+//     return
+//     plus
+//     minus
+//     divide
+//     multiply
+//     equal
+//     constructor (elDisplay,elNumber,elClean,elreturn,elPlus,elMinus,elDivide,elMultiply,elEqual){
+//         this.display = elDisplay;
+//         this.number = elNumber;
+//         this.clean = elClean;
+//         this.return = elreturn;
+//         this.plus = elPlus;
+//         this.minus = elMinus;
+//         this.divide = elDivide;
+//         this.multiply = elMultiply;
+//         this.equal = elEqual;
+//     }
+//     numDisplay(){
+
+//     }
+// }
+// let calc = new Calculator(
+//     display = document.querySelector('.text'),
     
     
 
-);
-num = document.querySelectorAll(".num");
-for (let i = 0; i < num.length; i++) {
-    num[i].addEventListener('click',function(){
-        let active = this.textContent
-        return active
-    })
+// );
+// num = document.querySelectorAll(".num");
+// for (let i = 0; i < num.length; i++) {
+//     num[i].addEventListener('click',function(){
+//         let active = this.textContent
+//         return active
+//     })
+// }
+// console.log(active);
+
+
+
+let numbers = document.querySelectorAll('.num')
+
+
+
+
+class Calculator {
+    display 
+    setDisplay(value) {
+
+    }
+    
+    numbers = () => {
+
+
+
+        setDisplay(num)
+    }
+
+    dot(){
+        setDisplay('.')
+    }
+
+    signs(){
+        setDisplay()
+    }
+
+    calculate(){
+
+    }
+    clear(){
+        display.value = null;
+    }
+    remove(){
+
+    }
+
 }
-console.log(active);
+
+let calculate = new Calculator();
+
+for(let number of numbers){
+    number.onclick = (event) => {
+        calculate.numbers(event)
+    }
+}
+
 
 
 
